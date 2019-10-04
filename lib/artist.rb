be5a9efa1bb9dc16694668c38e951b15365bffb2
Sharon Watkins <swatkins876@gmail.com>
2019-10-04 14:48:31 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+  attr_accessor :name, :song
+  
+  @@count = 0 
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+   @songs << song
+   song.artist = self
+   @@count += 1 
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+end 
---
+++ lib/author.rb
@@ -0,0 +1,33 @@
+class Author
+  
+  @@all = []
+  
+  attr_accessor :name 
+  
+  def initialize(name=nil)
+    @name = name 
+    @@all = self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def add_post(post)
+    post.author = self 
+  end 
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+
+  def posts
+    Post.all.select{|post| post.author == self}
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+  
+end 
