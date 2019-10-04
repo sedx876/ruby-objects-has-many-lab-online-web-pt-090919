2a5012ffcbca5849dc0335ea0c8d24e82e559c3c
Sharon Watkins <swatkins876@gmail.com>
2019-10-04 14:11:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,9 +13,7 @@ class Artist
   end 
   
   def add_song(song)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+    self.songs << song
   end
   
   def add_song_by_name(song_name)
